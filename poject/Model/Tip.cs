38d1b6f37eeb3a7ad4c7d50a79ffad6fc9247c5e
bobara <bobara188@gmail.com>
2024-02-19 13:00:46 +0200

    ggfgh

---
+++ poject/Model/Tip.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMVC.Model
+{
+    public class Tip
+    {
+        private double amount;
+        private double percent;
+
+        private double Amount 
+        {
+            get {  return amount; }
+            set {  amount = value; } 
+        }
+        public double Percent
+        {
+            get { return percent; }
+            set
+            {
+                if (value > 1)
+                {
+                    this.percent = value / 100.00;
+                }
+                else
+                {
+                    percent = value;
+                }
+            }
+        }
+        public Tip(double amount, double percent)
+        {
+            Amount = amount;
+            Percent = percent;
+        }
+        public Tip():this(0,0) { }
+
+        public double CalculateTip()
+        {
+            return Amount * Percent;
+        }
+
+        public double CalculateTotal()
+        {
+            return CalculateTip() + Amount;
+        }
+
+    }
+}
---
+++ poject/Views/Display.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMVC.Views
+{
+    public class Display
+    {
+        public double Percent { get; set; }
+        public double Amount { get; set; }
+        public double Total { get; set; }
+        public double TipAmount {  get; set; }
+
+        public Display() 
+        {
+            Percent = 0;
+            Amount = 0;
+
+            Total = 0;
+            TipAmount = 0;
+            GetValues();
+
+        }
+
+        public void GetValues()
+        {
+            Console.WriteLine("Enter values the amount of meal");
+            Amount = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the percent you want to tip");
+            Percent = double.Parse(Console.ReadLine());
+        }
+
+        public void ShowTipAndTotal()
+        {
+            Console.WriteLine("Your tip is {0:C}",TipAmount);
+            Console.WriteLine("Your total will{0:C}",Total);
+        }
+
+
+
+    }
+}
